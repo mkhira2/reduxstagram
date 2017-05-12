f4de2ce78ae1970f27ac10163a60e22394fd0739
Kenji Hirabayashi <mkhira2@gmail.com>
2017-05-12 00:04:55 -0500

    integrated store with react router

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,9 +1,9 @@
 import {combineReducers} from 'redux'
-import {routeReducer} from 'react-router-redux'
+import {routerReducer} from 'react-router-redux'
 
 import posts from './posts'
 import comments from './comments'
 
-const rootReducer = combineReducers({posts, comments, router: routerReducer})
+const routeReducer = combineReducers({posts, comments, routing: routerReducer})
 
-export default rootReducer
+export default routeReducer
